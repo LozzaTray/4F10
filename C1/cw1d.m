a61e61ef47014fb9980591f9ec97f80957a98435
Lawrence Tray <ltray99@gmail.com>
2020-11-05 18:39:17 +0000

    Finish d

--- C1/cw1d.m
+++ C1/cw1d.m
@@ -1,31 +1,17 @@
-meanfunc = [];                    % empty: don't use a mean function
-covfunc = {@covProd, {@covPeriodic, @covSEiso}};  % Periodic * SE
-likfunc = @likGauss;              % Gaussian likelihood
-
 % initial params
+covFunc = {@covProd, {@covPeriodic, @covSEiso}};
 cov = [-0.5, 0, 0, 2, 0];
-lik = 0;
 
-% define hyperparams
-hyp = struct('mean', [], 'cov', cov, 'lik', lik);
+n = 2000;
 
-n = 200;
-seed = 0.8;
-x = gpml_randn(seed, n, 1);
-%x = linspace(-5, 5, n)'; 
-K = feval(covfunc{:}, hyp.cov, x) + 1e-6 * eye(n);
-y = chol(K)'*x;
+seed = 0.9;
+x = linspace(-5, 5, n)';
+z = gpml_randn(seed, n, 1);
+K = feval(covfunc{:}, cov, x);
+K_pos_def = K + 1e-6 * eye(n);
+y = chol(K_pos_def)'*z;
 
-xs = linspace(-5, 5, n)';      % test input range
-[nlZ,dnlZ,post] = gp(hyp, @infGaussLik, meanfunc, covfunc, likfunc, x, y);
-[ymu,ys2,fmu,fs2,junk,post] = gp(hyp, @infGaussLik, meanfunc, covfunc, likfunc, x, post, xs);
-%[ymu, ys2, fmu, fs2] = gp(hyp, @infGaussLik, meanfunc, covfunc, likfunc, x, y, xs);
-f = [fmu+2*sqrt(fs2); flip(fmu-2*sqrt(fs2),1)];
-%fill([xs; flip(xs,1)], f, [7 7 7]/8)
-hold on;
-grid on;
-plot(xs, ymu);
-plot(x, y, '+');
+plot(x, y);
 xlabel('Input x');
 ylabel('Sampled Function f(x)');
-%legend('Sampled Function', 'Training Datapoints');
+title('Seed = 0.9');
